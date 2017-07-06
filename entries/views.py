a12c9c3951764af232fc347d2bcdfb5cf8b173b6
ahirnish <ahirnish@gmail.com>
2017-07-06 21:46:50 +0530

    adding databases

--- entries/views.py
+++ entries/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Entry
 
 def index(request):
-    return HttpResponse("My online journal homepage.")
+    entries = Entry.objects.all()
+    return render( request, 'entries/index.html', {'entries':entries} )
